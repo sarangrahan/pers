9a105a5c29f4259c139aecd603c2ac7a1ee16a88
sarangrahan <74257059+sarangrahan@users.noreply.github.com>
2022-08-23 20:12:44 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import Tippy from '@tippyjs/react';
 
 
 export default function Home() {
-  const { data: _repositories } = swr("/api/repos");
+  const { data: _repositories } = swr("/api/projects");
   const repositories = _repositories ? _repositories : null;
 
   return (
